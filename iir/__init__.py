df336f86bc07a0bebc232c0a66b7dcc118a8c8db
Peter Iannucci <iannucci@mit.edu>
2015-08-06 16:10:05 -0400

    Beautified code generation; removed dtype arguments.  Bandpass and Chebyshev filter support.

--- iir/__init__.py
+++ iir/__init__.py
@@ -21,10 +21,12 @@
 # THE SOFTWARE.
 
 import numpy as np
-import scipy.weave
-import mkfilter
+import weave
+from . import mkfilter
 
-# ContinuousIIRFilter(order, alpha, beta, gamma, dtype) constructs a filter with difference equation
+defaultChebyshevRipple_dB = -.021
+
+# IIRFilter(order, alpha, beta, gamma, dtype) constructs a filter with difference equation
 #
 # y[i] = gamma * sum(alpha[j] * x[i+j-order] for j in xrange(order+1)) +
 #        sum(beta[j] * y[i+j-order] for j in xrange(order))
@@ -34,55 +36,31 @@ import mkfilter
 #
 # whose initial state is all zeros and whose internal state representation has type dtype.
 
-def _iir_cpp_impl(order, alpha, beta, gamma, float=False):
-    suffix = 'f' if float else ''
-    impl = "for (int i=0; i<%d && i<N; i++)\n" % order
-    impl += "\ty(i) = x(i)*%.10e%s;\n" % (1./gamma, suffix)
-    impl += "for (int i=%d; i<N; i++)\n" % order
-    impl += "\ty(i) = " + " + ".join("%.10e%s*x(i%s)" % (alpha[i], suffix, str(i-order) if i!=order else "") for i in range(order+1))
-    impl += " + " + " + ".join("%.10e%s*y(i%s)" % (beta[i], suffix, str(i-order)) for i in range(order))
-    impl += ";\nfor (int i=0; i<N; i++)\n\ty(i) *= %.10e%s;" % (gamma, suffix)
-    return impl
-
-def _iir_cpp_impl_continuous(order, alpha, beta, gamma, float=False):
-    suffix = 'f' if float else ''
-    impl = "for (int i=0; i<%d; i++)\n" % order
-    impl += "{\n"
-    impl += "\tx(i) = x_hist(i);\n"
-    impl += "\ty(i) = y_hist(i);\n"
-    impl += "}\n"
-    impl += "for (int i=%d; i<N+%d; i++)\n" % (order, order)
-    impl += "\ty(i) = " + " + ".join("%.10e%s*x(i%s)" % (alpha[i], suffix, str(i-order) if i!=order else "") for i in range(order+1))
-    impl += " + " + " + ".join("%.10e%s*y(i%s)" % (beta[i], suffix, str(i-order)) for i in range(order))
-    impl += ";\n"
-    impl += "for (int i=0; i<%d; i++)\n" % order
-    impl += "{\n"
-    impl += "\tx_hist(i) = x(N+i);\n"
-    impl += "\ty_hist(i) = y(N+i);\n"
-    impl += "}\n"
-    impl += "for (int i=%d; i<N+%d; i++)\n" % (order, order)
-    impl += "\ty(i) *= %.10e;" % gamma
-    return impl
-
 class IIRFilter(object):
     def __init__(self, order, alpha, beta, gamma):
-        self.code = _iir_cpp_impl(order, alpha, beta, gamma)
-        self.codef = _iir_cpp_impl(order, alpha, beta, gamma, True)
-    def __call__(self, x):
-        x = np.array(x, copy=False)
-        y = np.zeros_like(x)
-        N = y.size
-        scipy.weave.inline(self.code if x.real.dtype==np.float64 else self.codef,
-                     ['N','x','y'], type_converters=scipy.weave.converters.blitz, verbose=0)
-        return y
-
-class ContinuousIIRFilter(object):
-    def __init__(self, order, alpha, beta, gamma, dtype):
-        single = (np.dtype(dtype).name in ['float32', 'complex64'])
-        self.code = _iir_cpp_impl_continuous(order, alpha, beta, gamma, single)
+        dtype = np.complex128
+        kernel = " + ".join(["{coeff}*x(i+{offset})".format(coeff=float.hex(alpha[i]), i=i, offset=i-order) for i in range(order+1)] +
+                            ["{coeff}*y(i+{offset})".format(coeff=float.hex(beta[i]), i=i, offset=i-order) for i in range(order)])
+        self.code = """
+        for (int i=0; i<{order}; i++)
+        {{
+            x(i) = x_hist(i);
+            y(i) = y_hist(i);
+        }}
+        for (int i={order}; i<N+{order}; i++)
+            y(i) = {kernel};
+        for (int i=0; i<{order}; i++)
+        {{
+            x_hist(i) = x(N+i);
+            y_hist(i) = y(N+i);
+        }}
+        for (int i={order}; i<N+{order}; i++)
+            y(i) *= {gamma};
+        """.format(order=order, kernel=kernel, gamma=float.hex(gamma))
         self.x_hist = np.zeros(order, dtype)
         self.y_hist = np.zeros(order, dtype)
         self.order = order
+        self([dtype(0)])
     def __call__(self, x):
         x = np.array(x, copy=False)
         N = x.size
@@ -90,11 +68,11 @@ class ContinuousIIRFilter(object):
         y = np.empty(N+self.order, x.dtype)
         x_hist = self.x_hist
         y_hist = self.y_hist
-        scipy.weave.inline(self.code, ['N','x_hist','y_hist','x','y'],
-                     type_converters=scipy.weave.converters.blitz, verbose=0)
+        weave.inline(self.code, ['N','x_hist','y_hist','x','y'],
+                     type_converters=weave.converters.blitz, verbose=0)
         return y[self.order:]
     def copy(self):
-        other = ContinuousIIRFilter.__new__(ContinuousIIRFilter)
+        other = IIRFilter.__new__(IIRFilter)
         other.code = self.code
         dtype = self.x_hist.dtype
         other.x_hist = np.zeros(self.order, dtype)
@@ -102,18 +80,22 @@ class ContinuousIIRFilter(object):
         other.order = self.order
         return other
 
-def MkFilter(method, type, order, alpha, continuous=False, dtype=None):
+def MkFilter(method, type, order, alpha, ripple=None):
     if not isinstance(alpha, (tuple, list)):
         alpha = (alpha,)
-    args = ('-%s' % method).split(' ') + ['-%s' % type, '-o', order, '-a'] + list(alpha)
-    order, alpha, beta, gamma = mkfilter.mkfilter(*args)
-    if not continuous:
-        return IIRFilter(order, alpha, beta, gamma)
-    else:
-        return ContinuousIIRFilter(order, alpha, beta, gamma, dtype)
+    args = ['-%s' % method]
+    if method == 'Ch':
+        if ripple is None:
+            ripple = defaultChebyshevRipple_dB
+        args.append(ripple)
+    args += ['-%s' % type, '-o', order, '-a'] + list(alpha)
+    return IIRFilter(*mkfilter.mkfilter(*args))
 
-def lowpass(freq, order=6, method='Bu', continuous=False, dtype=None):
-    return MkFilter(method, 'Lp', order, freq, continuous, dtype)
+def lowpass(freq, order=6, method='Bu', ripple=None):
+    return MkFilter(method, 'Lp', order, freq, ripple)
 
-def highpass(freq, order=6, method='Bu', continuous=False, dtype=None):
-    return MkFilter(method, 'Hp', order, freq, continuous, dtype)
+def highpass(freq, order=6, method='Bu', ripple=None):
+    return MkFilter(method, 'Hp', order, freq, ripple)
+
+def bandpass(freq1, freq2, order=6, method='Bu', ripple=None):
+    return MkFilter(method, 'Hp', order, (freq1, freq2), ripple)
